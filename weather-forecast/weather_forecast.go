980cf65c2b428c7fdec3adbe0590e3f24061dc55
Febil Aravindan <febil.aravindakshan@wallester.com>
2026-02-10 17:52:21 +0200

    weather forecast challange

--- weather-forecast/weather_forecast.go
+++ weather-forecast/weather_forecast.go
@@ -1,10 +1,14 @@
+//Package weather forcasts weather of the city.
 package weather
 
 var (
+//CurrentCondition represents the current condition and CurrentLocation is the current location.
 	CurrentCondition string
+//CurrentLocation represents the current location. 
 	CurrentLocation  string
 )
 
+//Forecast funtion will return the given cities weather. 
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
