7bc5c1f8a2ebb07edcf58048f8fc65c119607c97
Febil Aravindan <febil.aravindakshan@wallester.com>
2026-02-12 17:49:25 +0200

    better version of code

--- vehicle-purchase/vehicle_purchase.go
+++ vehicle-purchase/vehicle_purchase.go
@@ -1,43 +1,26 @@
 package purchase
 
-import s "strings"
-import "fmt"
-
 // NeedsLicense determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
 func NeedsLicense(kind string) bool {
-	var needLisence bool
-	if kind == "car" {
-		needLisence = true
-	} else if kind == "truck" {
-		needLisence = true
-	}
-	return needLisence
+	return kind == "car" || kind == "truck"
 }
 
 // ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
 func ChooseVehicle(option1, option2 string) string {
-	var car string
-	comparedInteger := s.Compare(option1, option2)
-	if comparedInteger == 1 {
-		car = option2
-	} else if comparedInteger == -1 {
-		car = option1
-	} else if comparedInteger == 0 {
-		car = option1
+	bttrChoiceMsg := " is clearly the better choice."
+	if option1 < option2 {
+		return option1 + bttrChoiceMsg
 	}
-	return fmt.Sprintf("%s is clearly the better choice.", car)
+	return option2 + bttrChoiceMsg
 }
 
 // CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
 func CalculateResellPrice(originalPrice, age float64) float64 {
-	var resellPrice float64
+	percent := 0.5
 	if age < 3 {
-		resellPrice = originalPrice * 0.8
-	} else if age >= 3 && age < 10 {
-		resellPrice = originalPrice * 0.7
-
-	} else if age >= 10 {
-		resellPrice = originalPrice * 0.5
+		percent = 0.8
+	} else if age < 10 {
+		percent = 0.7
 	}
-	return resellPrice
+	return originalPrice * percent
 }
